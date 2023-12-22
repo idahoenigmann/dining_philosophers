dc5a795bd86f83cff798579485ce360008bfd4e9
Ida Hoenigmann <idahoney@gmx.at>
2023-12-22 19:09:08 +0100

    add hungriness parameter

--- basic_event_based.py
+++ basic_event_based.py
@@ -58,21 +58,28 @@ def visualize_hungriness():
 
 class Philosopher:
 
-    def __init__(self, id):
+    def __init__(self, id, hungriness):
         self.state = "-"
         self.id = id
         self.log = []
-        self.hungriness = 0.0
+        if hungriness:
+            self.hungriness = 0.0
+        else:
+            self.hungriness = None
         self.meditate()
 
     def __str__(self):
-        return f"Philosopher {self.id}, State {self.state}, Hungriness {self.hungriness}"
+        if self.hungriness is None:
+            return f"Philosopher {self.id}, State {self.state}"
+        else:
+            return f"Philosopher {self.id}, State {self.state}, Hungriness {self.hungriness}"
 
     def meditate(self):
         self.state = "M"
         self.log.append(f"Meditating,{int(current_time)}\n")
         meditating_time = meditating_time_distribution(self.id, current_time, self.hungriness)
-        self.hungriness = increase_hungriness(self.hungriness, meditating_time)
+        if self.hungriness is not None:
+            self.hungriness = increase_hungriness(self.hungriness, meditating_time)
         add_event(meditating_time, self.get_left_chopstick)
 
     def get_left_chopstick(self):
@@ -84,7 +91,8 @@ class Philosopher:
         else:
             # could not get left chopstick, try again in 1 time unit
             add_event(1, self.get_left_chopstick)
-            self.hungriness = increase_hungriness(self.hungriness, 1)
+            if self.hungriness is not None:
+                self.hungriness = increase_hungriness(self.hungriness, 1)
 
     def get_right_chopstick(self):
         self.state = "R"
@@ -95,13 +103,15 @@ class Philosopher:
         else:
             # could not get right chopstick, try again in 1 time unit
             add_event(1, self.get_right_chopstick)
-            self.hungriness = increase_hungriness(self.hungriness, 1)
+            if self.hungriness is not None:
+                self.hungriness = increase_hungriness(self.hungriness, 1)
 
     def eat(self):
         self.state = "E"
         self.log.append(f"Eating,{int(current_time)}\n")
         eating_time = eating_time_distribution()
-        self.hungriness = decrease_hungriness(self.hungriness, eating_time)
+        if self.hungriness is not None:
+            self.hungriness = decrease_hungriness(self.hungriness, eating_time)
         add_event(eating_time, self.return_chopsticks)
 
     def return_chopsticks(self):
@@ -120,6 +130,7 @@ class Philosopher:
 if __name__ == "__main__":
     output_to_files_arg = ("-o" in sys.argv or "--output" in sys.argv)
     cnt_max_events_arg = ("-c" in sys.argv or "--count" in sys.argv)
+    hungriness_arg = ("--hungriness" in sys.argv)
 
     # parameters
     cnt_max_events = math.inf   # math.inf for simulation until deadlock
@@ -133,10 +144,13 @@ if __name__ == "__main__":
     cnt_events_until_deadlock = 100
 
     # initialize philosophers
-    philosophers = [Philosopher(id) for id in range(5)]
+    philosophers = [Philosopher(id, hungriness_arg) for id in range(5)]
 
     print("M ... meditating \t L/R ... getting left/right chopstick \t E ... eating \t - ... returning chopstick")
-    print(f"Time  :  State                Hungriness")
+    if hungriness_arg:
+        print(f"Time  :  State                Hungriness")
+    else:
+        print(f"Time  :  State")
     cnt_events = 0
 
     # variables for deadlock detection
@@ -146,12 +160,25 @@ if __name__ == "__main__":
     # deadlock is detected if nothing changes for cnt_events_until_deadlock events
     while cnt_last_state < cnt_events_until_deadlock and cnt_events < cnt_max_events:
         # show state
-        print(f"{current_time:6.2f}: {visualize_states()} \t {visualize_hungriness()}")
+        if hungriness_arg:
+            print(f"{current_time:6.2f}: {visualize_states()} \t {visualize_hungriness()}")
+        else:
+            print(f"{current_time:6.2f}: {visualize_states()}")
 
         cnt_events += 1
         current_time, function = get_event()
         # call function corresponding to event
         function()
+
+        # check if a philosopher died
+        if hungriness_arg:
+            someone_died = False
+            for p in philosophers:
+                if p.hungriness == 1:
+                    print(f"Philosopher {p.id} died from starvation.")
+                    someone_died = True
+            if someone_died:
+                break
 
         # update variables for deadlock checking
         if visualize_states() == last_state:
@@ -163,8 +190,10 @@ if __name__ == "__main__":
     if cnt_last_state >= cnt_events_until_deadlock:
         print(f"Simulation ended as deadlock is detected after {cnt_events} events. "
               f"In the last {cnt_events_until_deadlock} events nothing changed.")
-    else:
+    elif cnt_events >= cnt_max_events:
         print(f"Simulation ended after {cnt_events} events. No deadlock occurred.")
+    else:
+        print(f"Simulation ended after a philosopher died after {cnt_events} events.")
 
     if output_to_files_arg:
         for p in philosophers:
--- parameters.py
+++ parameters.py
@@ -11,7 +11,10 @@ strategy = "deadlock_possible"
 # random time distributions
 def meditating_time_distribution(id=None, time=None, hungriness=None):
     if strategy == "deadlock_possible":
-        return max(random.normal(loc=(-4*hungriness+5), scale=3), 0)
+        if hungriness is not None:
+            return max(random.normal(loc=(-4*hungriness+5), scale=3), 0)
+        else:
+            return max(random.normal(loc=(5), scale=3), 0)
     elif strategy == "no_deadlock":
         if (id + time) % 5 == 0 or (id + time) % 5 == 2:
             return 1
@@ -26,8 +29,8 @@ def eating_time_distribution():
 
 
 def increase_hungriness(curr_hungriness, time):
-    return min(curr_hungriness + time * 0.066666, 1)
+    return min(curr_hungriness + time * 0.04, 1)
 
 
 def decrease_hungriness(curr_hungriness, time):
-    return max(curr_hungriness - time * 0.166666, 0)
+    return max(curr_hungriness - time * 0.2, 0)
